401e8a8d98f5631b18266814650d6a112ea178f7
Saan-prog <ssandra6045@gmail.com>
2026-06-11 15:49:07 +0530

    fix: remove trailing slash from cors origin

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://task-manager-todo-application-frontend.onrender.com/"
+    "https://task-manager-todo-application-frontend.onrender.com"
     
   ],
   credentials: true
