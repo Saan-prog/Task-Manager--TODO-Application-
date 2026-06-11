42aaf1804bfb1d6fc5ea12ab8fa6d6a2995ac876
Saan-prog <ssandra6045@gmail.com>
2026-06-11 14:18:30 +0530

    fix: use dynamic port for render

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(errorHandler);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8090;
 
 app.listen(PORT, () => {
     console.log(`Server ruuning on port ${PORT}`);
